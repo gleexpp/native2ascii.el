597528101a650ceb048084e9d6123158fccd342e
GuoLi <gleexpp@gmail.com>
2012-02-04 16:57:46 +0800

    change encoding to utf-8

--- native2ascii.el
+++ native2ascii.el
@@ -5,13 +5,13 @@
   :group 'java
   :prefix 'string)
 
-(defcustom native2ascii-command-format "native2ascii"
+(defcustom native2ascii-command-format "native2ascii -encoding utf-8"
   "native2ascii command"
   :group 'native2ascii
   :type '(choice (const :tag "Default" "native2ascii")
 				 (string :tag "String")))
 
-(defcustom ascii2native-command-format "native2ascii -reverse"
+(defcustom ascii2native-command-format "native2ascii -encoding utf-8 -reverse"
   "ascii2native command"
   :group 'native2ascii
   :type '(choice (const :tag "Default" "native2ascii -reverse")
@@ -19,32 +19,34 @@
 
 (defun native2ascii (start end)
   (interactive "r")
-  (let ((coding-system-for-write 'shift_jis))
+  (let ((coding-system-for-write 'utf-8)
+        (coding-system-for-read 'utf-8))
 	(shell-command-on-region start end native2ascii-command-format nil t)))
 
 (defun ascii2native (start end)
   (interactive "r")
-  (let ((coding-system-for-read 'shift_jis))
+  (let ((coding-system-for-read 'utf-8)
+        (coding-system-for-write 'utf-8))
 	(shell-command-on-region start end ascii2native-command-format nil t)))
 
-(defun find-file-hook-ascii2native ()
-  (if (string-match "\\.properties\\(\\|_ja\\)$" buffer-file-name)
+(defun find-file-hook-ascii2native () 
+  (if (string-match "\\.properties\\(\\|_zh_CN\\)$" buffer-file-name)
 	  (progn
 		(buffer-disable-undo)
-		(set-buffer-file-coding-system 'sjis)
+		(set-buffer-file-coding-system 'utf-8)
 		(ascii2native (point-min) (point-max))
 		(set-buffer-modified-p nil)
 		(buffer-enable-undo))))
 
 (defun before-save-hook-native2ascii ()
-  (if (string-match "\\.properties\\(\\|_ja\\)$" buffer-file-name)
+  (if (string-match "\\.properties\\(\\|_zh_CN\\)$" buffer-file-name)
 	  (if (buffer-modified-p)
 		  (save-restriction
 			(widen)
 			(native2ascii (point-min) (point-max))))))
 
 (defun after-save-hook-native2ascii ()
-  (if (string-match "\\.properties\\(\\|_ja\\)$" buffer-file-name)
+  (if (string-match "\\.properties\\(\\|_zh_CN\\)$" buffer-file-name)
 	  (save-restriction
 	    (widen)
 		(undo-start)
